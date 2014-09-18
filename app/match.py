bf259b93732ceeb35454ee57b34011c2f131df9e
brisiacorona <brisiacorona@gmail.com>
2014-09-17 19:39:40 -0500

    tarea

--- app/match.py
+++ app/match.py
@@ -1,6 +1,65 @@
 # -*- coding: utf-8 -*-
 class Match:
-    def __init__(self,player1,player2,pacted_sets):
-        self.p1 = player1
-        self.p2 = player2
-        self.pacted_sets = pacted_sets
+	score = []
+	setss = {"1st": 0, "2nd": 1, "3rd": 2, "4th": 3, "5th": 4}
+
+	def __init__(self, player1, player2, pacted_sets):
+		self.p1 = player1
+		self.p2 = player2
+		self.pacted_sets = pacted_sets
+		self.txtWin = ""
+		self.wonp1 = 0
+		self.wonp2 = 0
+		del self.score[:]
+		self.score.append("0-0")
+		self.Win1 = "none"
+
+	def score_set(self):
+		self.Win()
+		return "{0} | {1}".format(self.txtWin, self.listascoreset())
+
+	def start_Win(self, playerwon):
+		if self.Win1 == "none":
+			self.Win1 = playerwon
+
+	def listascoreset(self):
+		listascore = ""
+		inicio = 0
+		for index in self.score:
+			if inicio == 0:
+				listascore = index
+				inicio += 1
+			else:
+				listascore = listascore + ", " + index
+		return listascore
+
+	def ordenarscoreganador(self, playerwon, scorej1, scorej2):
+		self.start_Win(playerwon)
+		if self.Win1 == playerwon:
+			return scorej1 + "-" + scorej2
+		else:
+			return scorej2 + "-" + scorej1
+
+	def save_set_won(self, player):
+		if(player == self.p1):
+			self.wonp1 += 1
+		else:
+			self.wonp2 += 1
+
+	def save_score_set(self, scorej1, scorej2, numberset, playerwon):
+		if(numberset == "1st"):
+			self.score[0] = self.ordenarscoreganador(playerwon, scorej1, scorej2)
+		else:
+			self.score.insert(self.setss.get(numberset), self.ordenarscoreganador(playerwon, scorej1, scorej2))
+
+	def Win(self):
+		numeroaum = 1
+		if int(self.pacted_sets) == 5:
+			numeroaum = 2
+		if((self.wonp1 + numeroaum) == int(self.pacted_sets)):
+			self.txtWin = self.p1 + " defeated " + self.p2
+		elif ((self.wonp2 + numeroaum) == int(self.pacted_sets)):
+			self.txtWin = self.p2 + " defeated " + self.p1
+		else:
+			self.txtWin = self.p1 + " plays with " + self.p2
+
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -4,32 +4,28 @@ import app.match as m
 
 
 @step(u'Given: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def given_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    world.match = m.Match(group1,group2,group3)
-
-
-@step(u'When: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def when_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    assert False, 'This step must be implemented'
-
+def given_group1_and_group2_start_a_match_to_group3_sets(step, player1, player2, sets):
+    world.match = m.Match(player1, player2, sets)
 
 @step(u'Then: I see score: "([^"]*)"')
-def then_i_see_score_group1(step, group1):
-    assert False, 'This step must be implemented'
+def then_i_see_score_group1(step, score):
+    assert world.match.score_set() == score, \
+        "Got %s" % world.match.score_set()
 
 
 @step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
-def when_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+def when_group1_won_the_group2_set_group3_group4(step, player1, numset, ptoPlayer1, ptoPlayer2):
+    world.match.save_set_won(player1)
+    world.match.save_score_set(ptoPlayer1, ptoPlayer2, numset, player1)
 
 
 @step(u'And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
-def and_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+def and_group1_won_the_group2_set_group3_group4(step, player1, numset, ptoPlayer1, ptoPlayer2):
+    world.match.save_set_won(player1)
+    world.match.save_score_set(ptoPlayer1, ptoPlayer2, numset, player1)
 
 
 @step(u'Then: The match score is: "([^"]*)"')
-def then_the_match_score_is_group1(step, group1):
-    assert False, 'This step must be implemented'
-
-
+def then_the_match_score_is_group1(step, score):
+    assert world.match.score_set() == score, \
+        "Got %s" % world.match.score_set()
